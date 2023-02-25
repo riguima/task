a5d418653430b7b9fdea8eddcfa076a53db9f833
ri-guima <richard.alexsander.guima@gmail.com>
2023-02-25 09:05:17 -0300

    refactor: add eval instead match case

--- task/main.py
+++ task/main.py
@@ -1,16 +1,20 @@
 from argparse import ArgumentParser
-from domain import add, rm
+from domain import *
+
+
+def run_command(args) -> None:
+    if args.command in ['rm']:
+        eval(f'{args.command}(int(args.value))')
+    else:
+        eval(f'{args.command}(args.value)')
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(prog='Task', description='Task taking')
     parser.add_argument('command', type=str)
-    parser.add_argument('value', type=str)
+    parser.add_argument('value')
     args = parser.parse_args()
-    match args.command:
-        case 'add':
-            add(args.value)
-        case 'rm':
-            rm(int(args.value))
-        case _:
-            print('Invalid command!')
+    try:
+        run_command(args)
+    except NameError:
+        print('Invalid command')
