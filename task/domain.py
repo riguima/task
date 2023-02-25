b4b0f1cb897c3e250f1254cddf3c76db695bfee1
ri-guima <richard.alexsander.guima@gmail.com>
2023-02-24 22:18:44 -0300

    tests: tests are passing

--- task/domain.py
+++ task/domain.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 import json
 import os
+from marshmallow import Schema, fields, post_load
 from pathlib import Path
 
 
@@ -12,30 +13,47 @@ class Task:
     created_at: datetime = datetime.now()
 
 
-def commit(tasks: list[Task], commits_dir: Path) -> None:
-    commit_number = len(os.listdir(commits_dir)) + 1
-    json.dump(jsonify(tasks), open(commits_dir / f'{commit_number}.json', 'w'))
+class TaskSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    created_at = fields.DateTime()
+
+    @post_load
+    def make_task(self, data: dict, **kwargs) -> Task:
+        return Task(**data)
 
 
-def checkout(commits_dir: Path, commit_number: int = -1) -> list[Task]:
-    filename = commits_dir / os.listdir(commits_dir)[commit_number]
-    return list(map(lambda t: Task(**t), json.load(open(filename, 'w'))))
+def commit(function: callable) -> callable:
+    def decorator(*args, **kwargs) -> list[Task]:
+        last_commit_number = len(os.listdir(get_commits_dir()))
+        filename = get_commits_dir() / f'{last_commit_number + 1}.json'
+        if last_commit_number == 0:
+            json.dump([], open(filename, 'w'))
+        tasks = function(*args, **kwargs)
+        json.dump([TaskSchema().dump(t) for t in tasks], open(filename, 'w'))
+        return tasks
+    return decorator
 
 
-def add(name: str, commits_dir: Path) -> Task:
-    tasks = checkout(commits_dir)
-    task = Task(len(tasks) + 1, name)
-    tasks.append(task)
-    commit(tasks, commits_dir)
-    return task
+def checkout(index: int = -1) -> list[Task]:
+    commits = list(map(lambda f: f.split('.')[0], os.listdir(get_commits_dir())))
+    filename = get_commits_dir() / f'{sorted(commits)[index]}.json'
+    return list(map(lambda t: TaskSchema().load(t), json.load(open(filename, 'r'))))
 
 
-def rm(id: int, commits_dir: Path) -> Task:
-    tasks = checkout(commits_dir)
-    task = tasks.pop(id)
-    commit(tasks, commits_dir)
-    return task
+@commit
+def add(name: str) -> list[Task]:
+    tasks = checkout()
+    tasks.append(Task(len(tasks) + 1, name))
+    return tasks
 
 
-def jsonify(tasks: list[Task]) -> list[dict]:
-    return list(map(lambda t: t.__dict__, tasks))
+@commit
+def rm(index: int) -> list[Task]:
+    tasks = checkout()
+    tasks.pop(index)
+    return tasks
+
+
+def get_commits_dir() -> Path:
+    return Path(os.getenv('COMMITS_DIR', Path.home() / 'commits'))
